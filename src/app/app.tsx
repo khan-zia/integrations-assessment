5b7cac820e7e5349ec08c1510419d0a75055bd27
haakonjackfloat <121900867+haakonjackfloat@users.noreply.github.com>
2023-07-26 13:17:06 -0700

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+export function App() {
+  return (
+    <div>
+      Component goes here!
+    </div>
+  );
+}
+
+export default App;
