d053db5bdabe719bd90a0929b7cd37a73da2db59
Zia Ur Rehman Khan <zmajrohi323@gmail.com>
2023-08-01 23:28:20 +0500

    Update app.tsx to display the UI.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,12 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import { IntegrationInput } from './components/integrationInput';
 
 export function App() {
-  return (
-    <div>
-      Component goes here!
-    </div>
-  );
+	return (
+		<div className={styles.wrapper}>
+			<IntegrationInput />
+		</div>
+	);
 }
 
 export default App;
