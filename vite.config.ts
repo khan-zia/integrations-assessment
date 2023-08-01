1fee77bba7e27daf1901ba9c287347d622fa0b84
Zia Ur Rehman Khan <zmajrohi323@gmail.com>
2023-08-01 23:20:28 +0500

    fonts + typescript support for css modules + svg plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,33 +2,42 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  cacheDir: './node_modules/.vite/interfaces-interview-assignment',
+	cacheDir: './node_modules/.vite/interfaces-interview-assignment',
 
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
+	server: {
+		port: 4200,
+		host: 'localhost',
+	},
 
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
+	preview: {
+		port: 4300,
+		host: 'localhost',
+	},
 
-  plugins: [
-    react(),
-    viteTsConfigPaths({
-      root: './',
-    }),
-  ],
+	plugins: [
+		react(),
+		viteTsConfigPaths({
+			root: './',
+		}),
+		svgr({
+			svgrOptions: {
+				plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+				svgoConfig: {
+					plugins: ['preset-default', 'prefixIds'],
+				},
+			},
+		}),
+	],
 
-  test: {
-    globals: true,
-    cache: {
-      dir: './node_modules/.vitest',
-    },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-  },
+	test: {
+		globals: true,
+		cache: {
+			dir: './node_modules/.vitest',
+		},
+		environment: 'jsdom',
+		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+	},
 });
